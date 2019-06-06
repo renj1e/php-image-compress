5c5794e933516d1f95688aa087366992b558838c
Renjel Cabaral <renjelc@gmail.com>
2019-06-06 13:45:04 +0800

    New class update

--- src/CompressorIO.php
+++ src/CompressorIO.php
@@ -1,152 +1,193 @@
 <?php
 namespace renj1e\phpImgCompress;
 
-// GLOBAL VARIABLES
-define("BACKUP_FOLDER", getcwd() . DIRECTORY_SEPARATOR . 'backup');
-define("COOKIE_FILE", __DIR__ . DIRECTORY_SEPARATOR . 'cookie.txt');
-
 class CompressorIO
 {
 
-	public $backup = true;
-	public $console = false;
-
-	public function fileSave($Source, $fildeDir)
-	{
-		$ch = curl_init();
-
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($ch, CURLOPT_HEADER, false);
-		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-		$headers = [
-			'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-			'Accept-Language: tr-TR,tr;q=0.8,en-US;q=0.6,en;q=0.4	',
-			'Cache-Control: no-cache',
-			'Upgrade-Insecure-Requests: 1', //Your referrer address
-		];
-
-		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-		curl_setopt($ch, CURLOPT_COOKIEJAR, \COOKIE_FILE);  //could be empty, but cause problems on some hosts
-		curl_setopt($ch, CURLOPT_COOKIEFILE, \COOKIE_FILE);  //could be empty, but cause problems on some hosts
-		curl_setopt($ch, CURLOPT_URL, $Source);
-
-		$out = curl_exec($ch);
-
-		$fp = fopen($fildeDir, 'w');
-		fwrite($fp, $out);
-		fclose($fp);
-
-		curl_close($ch);
-
-	}
-
-	private function doIt($filename)
-	{
-		$delimiter = '-------------' . uniqid();
-		$data = '';
-		$data .= "--" . $delimiter . "\r\n";
-		$data .= 'Content-Disposition: form-data; name="files[]";  filename="' . basename($filename) . '"' . "\r\n";
-		$data .= 'Content-Type: ' . exif_imagetype($filename) . "\r\n\r\n";
-		$data .= file_get_contents($filename);
-		$data .= "\r\n" . "--" . $delimiter . "--\r\n";
-		$str = $data;
-// set up cURL
-		$ch = curl_init('https://compressor.io/server/Lossy.php');
-		curl_setopt_array($ch, [
-			CURLOPT_HEADER         => false,
-			CURLOPT_RETURNTRANSFER => true,
-			CURLOPT_POST           => true,
-			CURLOPT_HTTPHEADER     => [ // we need to send these two headers
-				'Content-Type: multipart/form-data; boundary=' . $delimiter,
-				'Content-Length: ' . strlen($str),
-			],
-			CURLOPT_POSTFIELDS     => $data,
-			CURLOPT_COOKIEJAR      => \COOKIE_FILE,
-			CURLOPT_COOKIEFILE     => \COOKIE_FILE,
-			CURLOPT_USERAGENT      => "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.3) Gecko/20070309 Firefox/2.0.0.3",
-		]);
-		$ress = curl_exec($ch);
-		curl_close($ch);
-
-		return json_decode($ress, true);
-	}
-
-	public function rsearch($folder, $pattern)
-	{
-		$dir = new \RecursiveDirectoryIterator($folder);
-		$ite = new \RecursiveIteratorIterator($dir);
-		$files = new \RegexIterator($ite, $pattern, \RegexIterator::GET_MATCH);
-		$fileList = [];
-		foreach ($files as $file) {
-			$fileList = array_merge($fileList, $file);
-		}
-
-		return $fileList;
-	}
-
-	public function findFolder($extensions = "png|jpg|jpeg|gif")
-	{
-
-		return $this->rsearch(getcwd(), '/.*\.(?:' . $extensions . ')/');
-	}
-
-	public function compress($files)
-	{
-
-		$result = [];
-
-		foreach ($files as $fileDir) {
-
-			$filename = basename($fileDir);
-
-			// without backup folder.
-			if (strpos($fileDir, \BACKUP_FOLDER) !== false) {
-				continue;
-			}
-
-			$compressedResult = $this->doIt($fileDir);
-
-			if (!empty($compressedResult['files'][0]['url'])) {
-				if ($compressedResult['files'][0]['size'] <= $compressedResult['files'][0]['sizeAfter']) {
-					$message = "{$filename} is already compressed.\r\n";
-					if ($this->console) {
-						echo $message;
-					} else {
-						$result[] = $message;
-					}
-				} else {
-
-					// backup.
-					if ($this->backup) {
-
-						$backupFolder = str_replace(getcwd(), \BACKUP_FOLDER, $fileDir);
-
-						if (!file_exists(dirname($backupFolder)))
-							mkdir(dirname($backupFolder), 0777, true);
-
-						if (!copy($fileDir, $backupFolder)) {
-							$message = "$fileDir can not copied...\n";
-							if ($this->console) {
-								echo $message;
-							} else {
-								$result[] = $message;
-							}
-						}
-					}
-
-					$this->fileSave($compressedResult['files'][0]['url'], $fileDir);
-					$message = "{$filename} is compressed. Before Size: {$compressedResult['files'][0]['size']}B, After Size: {$compressedResult['files'][0]['sizeAfter']}B\r\n";
-					if ($this->console) {
-						echo $message;
-					} else {
-						$result[] = $message;
-					}
-				}
-			}
-		}
-
-		return $result;
-	}
+    // @var file_url
+    protected $file_url;
+    // @var new_name_image
+    protected $new_name_image;
+    // @var quality
+    protected $quality;
+    // @var quality
+    protected $pngQuality;
+    
+    // @var destination
+    protected $destination;
+    // @var image_size
+    protected $image_size;
+    
+    // @var image_data
+    protected $image_data;
+    
+    // @var image_mime
+    protected $image_mime;
+    
+    // @var array_img_types
+    protected $array_img_types;
+    
+    public function __construct($file_url, $new_name_image, $quality, $pngQuality, $destination = null) {
+        $this->set_file_url($file_url);
+        $this->set_new_name_image($new_name_image);
+        $this->set_quality($quality);
+        $this->set_destination($destination);
+    }
+    function get_file_url() {
+        return $this->file_url;
+    }
+    function get_new_name_image() {
+        return $this->new_name_image;
+    }
+    function get_quality() {
+        return $this->quality;
+    }
+    function get_pngQuality() {
+        return $this->pngQuality;
+    }
+    function set_file_url($file_url) {
+        $this->file_url = $file_url;
+    }
+    function set_new_name_image($new_name_image) {
+        $this->new_name_image = $new_name_image;
+    }
+    function set_quality($quality) {
+        $this->quality = $quality;
+    }
+    function set_pngQuality($pngQuality) {
+        $this->pngQuality = $pngQuality;
+    }
+    
+    function get_destination() {
+        return $this->destination;
+    }
+    function set_destination($destination) {
+        $this->destination = $destination;
+    }
+    
+    /**
+     * Function to compress image
+     * @return boolean
+     * @throws Exception
+     */
+    public function compress_image(){
+        
+        //Send image array
+        $array_img_types = array('image/gif', 'image/jpeg', 'image/pjpeg', 'image/png', 'image/x-png');
+        $new_image = null;
+        $last_char = null;
+        $image_extension = null;
+        $destination_extension = null;
+        $png_compression = null;
+        $maxsize = 12485760;
+        
+        try{
+            
+            //If not found the file
+            if(empty($this->file_url) && !file_exists($this->file_url)){
+                throw new Exception('Please inform the image!');
+                return false;
+            }
+            
+            //Get image width, height, mimetype, etc..
+            $image_data = getimagesize($this->file_url);
+            //Set MimeType on variable
+            $image_mime = $image_data['mime'];
+            
+            //Verifiy if the file is a image
+            if(!in_array($image_mime, $array_img_types)){
+                throw new Exception('Please send a image!');
+                return false; 
+            }
+            
+            //Get file size
+            $image_size = filesize($this->file_url);
+                                    
+            //if image size is bigger than 5mb
+            if($image_size >= $maxsize){
+                throw new Exception('Please send a imagem smaller than 5mb!');
+                return false;
+            }
+            
+            //If not found the destination
+            if(empty($this->new_name_image)){
+                throw new Exception('Please inform the destination name of image!');
+                return false;
+            }
+            
+            //If not found the quality
+            if(empty($this->quality)){
+                throw new Exception('Please inform the quality!');
+                return false;
+            }
+            //If not found the png quality
+            $png_compression = (!empty($this->pngQuality)) ? $this->pngQuality : 9 ;
+            
+            $image_extension = pathinfo($this->file_url, PATHINFO_EXTENSION);
+            //Verify if is sended a destination file name with extension
+            $destination_extension = pathinfo($this->new_name_image, PATHINFO_EXTENSION); 
+            //if empty
+            if(empty($destination_extension)){
+                $this->new_name_image = $this->new_name_image.'.'.$image_extension;
+            }
+            
+            //Verify if folder destination isn´t empty
+            if(!empty($this->destination)){
+                
+                //And verify the last one element of value
+                $last_char = substr($this->destination, -1);
+                
+                if($last_char !== '/'){
+                    $this->destination = $this->destination.'/';
+                }
+            }
+            
+            //Switch to find the file type
+            switch ($image_mime){
+                //if is JPG and siblings
+                case 'image/jpeg':
+                case 'image/pjpeg':
+                    //Create a new jpg image
+                    $new_image = imagecreatefromjpeg($this->file_url);
+                    imagejpeg($new_image, $this->destination.$this->new_name_image, $this->quality);
+                    break;
+                //if is PNG and siblings
+                case 'image/png':
+                case 'image/x-png':
+                    //Create a new png image
+                    $maxImgWidth = 900;
+                    // create image from posted file
+                    $src = imagecreatefrompng($this->file_url);
+                    // get original size of uploaded image
+                    list($width, $height) = getimagesize($this->file_url);
+                    if ($width > $maxImgWidth) 
+                    {
+                        $newwidth = $maxImgWidth;
+                        $newheight = ($height / $width) * $newwidth;
+                        $newImage = imagecreate($newwidth, $newheight);
+                        imagecopyresampled($newImage, $src, 0, 0, 0, 0, $newwidth, $newheight, $width, $height);
+                        imagepng($newImage, $this->destination.$this->new_name_image, $png_compression);
+                        imagedestroy($src);
+                        imagedestroy($newImage);
+                        $resizedFlag = true;
+                    }
+                    break;
+                // if is GIF
+                case 'image/gif':
+                    //Create a new gif image
+                    $new_image = imagecreatefromgif('conteudo/'.$this->file_url);
+                    imagealphablending($new_image, false);
+                    imagesavealpha($new_image, true);
+                    imagegif($new_image, $this->destination.$this->new_name_image);
+            }
+            
+        } catch (Exception $ex) {
+            return $ex->getMessage();
+        }
+        
+        //Return the new image resized
+        return $this->new_name_image;
+        
+    }
 }
-
 ?>
